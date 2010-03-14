e84fc3542065a9b1bd27172662a7dd9ef5180745
Dimitris Moraitis <dimo@unweb.me>
2010-03-14 02:00:50 +0000

    display document action icons
    svn path=/plumi.skin/trunk/; revision=112947

--- plumi/skin/browser/viewlets.py
+++ plumi/skin/browser/viewlets.py
@@ -1,3 +1,6 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from collective.plonebookmarklets.browser.viewlets import BookmarkletsActionsViewlet as BAViewlet
 
+class BookmarkletsActionsViewlet(BAViewlet):
+    render = ViewPageTemplateFile("templates/document_actions.pt")
