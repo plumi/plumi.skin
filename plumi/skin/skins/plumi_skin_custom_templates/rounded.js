572ca687844de1223acab16b3ac142225201d846
Dimitris Moraitis <dimo@unweb.me>
2010-03-16 20:25:47 +0000

    add upload indicator
    svn path=/plumi.skin/trunk/; revision=113216

--- plumi/skin/skins/plumi_skin_custom_templates/rounded.js
+++ plumi/skin/skins/plumi_skin_custom_templates/rounded.js
@@ -1,5 +1,13 @@
 jq(document).ready(function(){
 
+  jq('#plumivideo-base-edit').submit(function(){            
+    jq('form#plumivideo-base-edit').find('#video_file_file').each(function() {
+        var loading_html = '<p>Please be patient while your file is uploading. It may take a long time. Thanks.</p> <img src="ajax-loader.gif"/>';
+        jq('#video_file_file').after(loading_html);
+        alert("You are about to start uploading. Please be patient, as this may take some time. Don't close your browser or click 'Save' again. Thanks!");
+    });
+  });
+        
   jq('#toggleBookmarks').click(function() {
     if (jq('#toggledBookmarks').is(":hidden"))
  	{
