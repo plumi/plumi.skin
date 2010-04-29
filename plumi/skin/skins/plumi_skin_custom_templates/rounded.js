22b3b611aa5bcece6c987c23d975b62124eb597e
Dimitris Moraitis <dimo@unweb.me>
2010-04-29 13:02:29 +0000

    fix issue #394
    svn path=/plumi.skin/trunk/; revision=116426

--- plumi/skin/skins/plumi_skin_custom_templates/rounded.js
+++ plumi/skin/skins/plumi_skin_custom_templates/rounded.js
@@ -1,12 +1,12 @@
 jq(document).ready(function(){
-
   jq('#plumivideo-base-edit').submit(function(){            
     jq('form#plumivideo-base-edit').find('#video_file_file').each(function() {
-        if (jq('#video_file_upload').is(":checked")) {
+        if (!jq('#video_file_nochange').is(":checked")) {
             var loading_html = '<p>Please be patient while your file is uploading. It may take a long time. Thanks.</p> <img src="ajax-loader.gif"/>';
             jq('#video_file_file').after(loading_html);
             alert("You are about to start uploading. Please be patient, as this may take some time. Don't close your browser or click 'Save' again. Thanks!");
         }
+        
     });
   });
         
