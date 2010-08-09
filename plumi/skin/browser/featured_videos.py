0f36c4651f4ef9e69b88b0889e180052b17ec9d7
clopy <clopy@localhost>
2010-08-09 14:12:25 +0000

    Feature latest modified featured video
    svn path=/plumi.skin/trunk/; revision=123199

--- plumi/skin/browser/featured_videos.py
+++ plumi/skin/browser/featured_videos.py
@@ -47,7 +47,7 @@ class FeaturedVideosPage( CategoriesProvider ):
     @property
     def featured_items(self):
         filtering = dict(portal_type='PlumiVideo',
-                         sort_on='effective',
+                         sort_on='modified',
                          sort_order='reverse',
                          review_state='featured',
                          limit=self.limit_featured)
@@ -58,7 +58,7 @@ class FeaturedVideosPage( CategoriesProvider ):
     @property
     def news_and_events(self):
         filtering = dict(portal_type=['News Item','Event'],
-                         sort_on='effective',
+                         sort_on='modified',
                          sort_order='reverse',
                          review_state='featured',
                          limit=1)
@@ -67,7 +67,7 @@ class FeaturedVideosPage( CategoriesProvider ):
     @property
     def latest_videos(self):
         filtering = dict(portal_type='PlumiVideo',
-                         sort_on='effective',
+                         sort_on='modified',
                          sort_order='reverse',
                          review_state=['published','featured'],
                          limit=self.limit_latest)
@@ -78,7 +78,7 @@ class FeaturedVideosPage( CategoriesProvider ):
     @property
     def callouts(self):
         filtering = dict(portal_type=['PlumiCallOut'],
-                         sort_on='effective',
+                         sort_on='modified',
                          sort_order='reverse',
                          review_state='featured',
                          limit=1)
