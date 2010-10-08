719bfe5a229a0aa520daebf1dff0d84113ab32af
clopy <clopy@localhost>
2010-10-08 16:07:16 +0000

    fix video listing order
    svn path=/plumi.skin/trunk/; revision=145810

--- plumi/skin/browser/adapters.py
+++ plumi/skin/browser/adapters.py
@@ -27,13 +27,13 @@ class OrderedFolderVideosProvider( object ):
         """It would be much better not to count on the acquisition and
         old python script, and recode this part.
         """
-	if context.portal_type == 'PlumiVideoFolder':
+	if context.portal_type == 'Plumi Video Folder':
 		path = "/".join(context.getPhysicalPath())
 	
 		content_filter = dict()	
 		content_filter['path'] = path 
 		content_filter['portal_type']='PlumiVideo'
-		content_filter['sort_on'] = 'created'
+		content_filter['sort_on'] = 'effective'
 		content_filter['sort_order'] = 'reverse'
 		self.videos = context.getFolderContents(batch=True,contentFilter=content_filter)
 
