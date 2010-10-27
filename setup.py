df824e545ab1e40d6a3f48b79a904377d17473c7
Dimitris Moraitis <dimo@unweb.me>
2010-10-27 20:57:18 +0000

    Prepare plumi.skin 0.13.
    svn path=/plumi.skin/trunk/; revision=225429

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.12'
+version = '0.13'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
