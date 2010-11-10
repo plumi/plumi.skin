a720a5e096c545eab4ef6cf1e1c9d1223230b9d5
Dimitris Moraitis <dimo@unweb.me>
2010-11-10 15:03:36 +0000

    remove bookmarklet viewlet
    svn path=/plumi.skin/trunk/; revision=226478

--- plumi/skin/browser/viewlets.py
+++
@@ -1,6 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
-from collective.plonebookmarklets.browser.viewlets import BookmarkletsActionsViewlet as BAViewlet
-
-class BookmarkletsActionsViewlet(BAViewlet):
-    render = ViewPageTemplateFile("templates/document_actions.pt")
