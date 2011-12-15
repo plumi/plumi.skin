70cad1012cb0f5f27f7cd1396338e2442ccc827f
Dimitris Moraitis <dimo@unweb.me>
2011-12-15 22:21:20 +0200

    move hasImageAndCaptionForNews index from plumi.content to plumi.skin

---
+++ plumi/indexes.py
@@ -0,0 +1,20 @@
+"""This package adds extensions to portal_catalog.
+"""
+import logging
+from Products.ATContentTypes.interface.news import IATNewsItem
+
+@indexer(IATNewsItem)
+def hasImageAndCaptionForNews(object,**kw):
+    logger=logging.getLogger('plumi.skin.indexes')
+    logger.info('hasImageAndCaptionForNews - have %s ' % object )
+    
+    img = object.getImage()
+    #check that the image is set
+    if img is not None and img is not '':
+        caption = object.getImageCaption() or u''
+        md = {'image': True, 'caption': caption }
+    else:
+        md = {'image': False, 'caption': u''}
+
+    logger.info(' hasImageAndCaptionForNews returning %s  . thumbnail object is %s' % (md, object.getImage()))
+    return md
