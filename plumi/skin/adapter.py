7607840c93e6843c547e69ea2956656858bb7090
Clopy <clopy@unweb.me>
2012-12-04 21:08:32 +0200

    add activities,interests,formats to author

--- plumi/skin/adapter.py
+++ plumi/skin/adapter.py
@@ -8,3 +8,21 @@ class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
     def set_author_paypal(self, value):
         return self.context.setMemberProperties({'author_paypal': value})
     author_paypal = property(get_author_paypal, set_author_paypal)
+
+    def get_genre_interests(self):
+        return self.context.getProperty('genre_interests', '')
+    def set_genre_interests(self, value):
+        return self.context.setMemberProperties({'genre_interests': value})
+    genre_interests = property(get_genre_interests, set_genre_interests)
+
+    def get_activities(self):
+        return self.context.getProperty('activities', '')
+    def set_activities(self, value):
+        return self.context.setMemberProperties({'activities': value})
+    activities = property(get_activities, set_activities)
+
+    def get_media_formats(self):
+        return self.context.getProperty('media_formats', '')
+    def set_media_formats(self, value):
+        return self.context.setMemberProperties({'media_formats': value})
+    media_formats = property(get_media_formats, set_media_formats)
--- plumi/skin/userdataschema.py
+++ plumi/skin/userdataschema.py
@@ -3,6 +3,40 @@ from zope import schema
 
 from plone.app.users.userdataschema import IUserDataSchemaProvider
 from plone.app.users.userdataschema import IUserDataSchema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from Products.CMFCore.utils import getToolByName
+from five import grok
+
+@grok.provider(IContextSourceBinder)
+def genres(context):
+    pv = getToolByName(context.context, 'portal_vocabularies')
+    voc_genres = pv.getVocabularyByName('video_genre')
+    terms = []
+    voc_terms = voc_genres.getDisplayList(context).items()
+    terms = [SimpleTerm(value=term[0], token=term[0], title=term[1])
+                     for term in voc_terms]
+    return SimpleVocabulary(terms)
+
+@grok.provider(IContextSourceBinder)
+def activities(context):
+    pv = getToolByName(context.context, 'portal_vocabularies')
+    voc_activities = pv.getVocabularyByName('video_categories')
+    terms = []
+    voc_terms = voc_activities.getDisplayList(context).items()
+    terms = [SimpleTerm(value=term[0], token=term[0], title=term[1])
+                     for term in voc_terms]
+    return SimpleVocabulary(terms)
+
+@grok.provider(IContextSourceBinder)
+def formats(context):
+    pv = getToolByName(context.context, 'portal_vocabularies')
+    voc_formats = pv.getVocabularyByName('submission_categories')
+    terms = []
+    voc_terms = voc_formats.getDisplayList(context).items()
+    terms = [SimpleTerm(value=term[0], token=term[0], title=term[1])
+                     for term in voc_terms]
+    return SimpleVocabulary(terms)
 
 class UserDataSchemaProvider(object):
     implements(IUserDataSchemaProvider)
@@ -18,5 +52,23 @@ class IEnhancedUserDataSchema(IUserDataSchema):
     """
     author_paypal = schema.TextLine(
         title=u'Paypal Id',
+        required=False,
+    )
+
+    genre_interests = schema.List(
+        title=u'Interests',
+        value_type=schema.Choice(source=genres),
+        required=False,
+    )
+
+    activities = schema.List(
+        title=u'Activities',
+        value_type=schema.Choice(source = activities),
+        required=False,
+    )
+
+    media_formats = schema.List(
+        title=u'Media Formats',
+        value_type=schema.Choice(source = formats),
         required=False,
     )
